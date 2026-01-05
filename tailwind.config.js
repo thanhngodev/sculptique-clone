07b05cf3f4a57c8afe6c9b23802aa66724ace54c
thanhngodev <thanhngodev@gmail.com>
2026-01-05 23:03:54 +0700

    init source

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2f6f4e",
+        secondary: "#f6f1ea",
+        accent: "#e5b45b",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
